383fdb88a7a73cfa4931e845deff2b5eea903632
ArtemSh13 <mozg1303@ya.ru>
2024-04-08 00:06:45 +0300

    Really do exercise #2

---
+++ exercise_2.py
@@ -0,0 +1,9 @@
+# Comments allow to understand the code.
+# Python ignores lines after #
+
+print("It's my program") # and this comment is ignored
+
+# You can "turn off" parts of the code:
+# print("The code doesn't work")
+
+print("The code works")
