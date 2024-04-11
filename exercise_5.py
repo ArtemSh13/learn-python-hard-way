435f1e833df600f10e39c9fd89c1b02a90daf69c
ArtemSh13 <mozg1303@ya.ru>
2024-04-11 11:52:29 +0300

    Do exercise #5

---
+++ exercise_5.py
@@ -0,0 +1,17 @@
+my_name = 'ArtemSh13'
+my_age = 27
+my_height = 189 # cm
+my_weight = 100 # kg
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Blond'
+
+print(f"Let's talk about a person named {my_name}")
+print(f"His height is {my_height} cm or {my_height / 100} m")
+print(f"His weight is {my_weight} kg or {my_weight / 1_000} tonnes")
+print("Actually it's not that much")
+print(f"He has {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth usually are {my_teeth}, although he likes to drink coffee")
+# this line of code is quite difficult, make no mistake!
+total = my_age + my_height + my_weight
+print(f"If I will add {my_age}, {my_height} and {my_weight}, I'll get {total}")
