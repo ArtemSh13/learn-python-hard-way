00f23a9a0eb5727a2f699ac3af576e4c364be2d5
ArtemSh13 <mozg1303@ya.ru>
2024-04-18 00:12:36 +0300

    Do exercise #9

---
+++ exercise_9.py
@@ -0,0 +1,14 @@
+# A little unusual code - write it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("These are the days of the week: ", days)
+print("And these are the months: ", months)
+
+print("""
+What's happening?
+Three double quotes are used.
+We can print as much text as we want.
+Even 4 lines, 5 or 6.
+""")
