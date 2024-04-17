8eb7cf2bc58903677c661405eb5d2d8e6b896c34
ArtemSh13 <mozg1303@ya.ru>
2024-04-18 00:41:48 +0300

    Do exercise #10

---
+++ exercise_10.py
@@ -0,0 +1,15 @@
+d_artagnan = "\tMy name is d'Artagnan."
+athos = "This line is\ndivided into two"
+porthos = "I'm \\ - \\ a musketeer!"
+
+aramis = """
+Hints:
+\t\u2705 The Count of Gascony
+\t\u2705 Related to My Lady
+\t\u2705 The Baron\n\t\u2705 General of the Order
+"""
+
+print(d_artagnan)
+print(athos)
+print(porthos)
+print(aramis)
