949b97dde872e1e2b57dbbef7809dee012b6c31f
ArtemSh13 <mozg1303@ya.ru>
2024-04-18 12:07:37 +0300

    Do exercise #11

---
+++ exercise_11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = int(input())
+print("What is your height?", end=' ')
+height = int(input())
+print("What is your weight?", end=' ')
+weight = int(input())
+
+print(f"So, you're {age} years old, your height is {height} cm and your weight is {weight} kg.")
