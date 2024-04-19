913c3272dc7055e2ff2b657f10ad6efa916e9312
ArtemSh13 <mozg1303@ya.ru>
2024-04-19 14:04:08 +0300

    Do exercise #13

---
+++ exercise_13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+# Read the "Run result" chapter to understand
+# how to run this script
+script, first, second, third = argv
+print("This script is named: ", script)
+print("My first variable is named: ", first)
+print("My second variable is named: ", second)
+print("My third variable is named: ", third)
