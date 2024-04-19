477f0c98414f11982b2a09c0d13032f5809926b6
ArtemSh13 <mozg1303@ya.ru>
2024-04-19 14:09:10 +0300

    Do additional exercise #13

--- exercise_13.py
+++ exercise_13.py
@@ -3,7 +3,9 @@ from sys import argv
 # Read the "Run result" chapter to understand
 # how to run this script
 script, first, second, third = argv
+fourth = input("Enter a value of the 4th variable: ")
 print("This script is named: ", script)
 print("My first variable is named: ", first)
 print("My second variable is named: ", second)
 print("My third variable is named: ", third)
+print("My fourth variable is named: ", fourth)
