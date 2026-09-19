a1df912683b6db5b5cb12e253f19bc95d7e2633e
ArtemSh13 <mozg1303@ya.ru>
2026-09-19 14:08:41 +0300

    Change exercise dir format

--- create_exercise.sh
+++ create_exercise.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ "$1" = "file" ]; then
-     touch "exercise_${2}.py"
-fi
-if [ "$1" = 'dir' ]; then
-  mkdir "exercise_${2}"
-  touch "exercise_${2}/exercise_${2}.py"
-fi
+mkdir "ex${1}"
+touch "ex${1}/ex${1}.py"
 exit 0
